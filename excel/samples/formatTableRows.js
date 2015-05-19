0c793a13ae8f783bb73276ddf72f4c332bcbf98f
TristanD-MSFT <tristand@microsoft.com>
2015-05-18 23:28:55 -0700

    Update formatTableRows.js

--- excel/samples/formatTableRows.js
+++ excel/samples/formatTableRows.js
@@ -1,24 +1,20 @@
 var ctx = new Excel.ExcelClientContext();
-var table = ctx.workbook.tables.getItem("Table1");
-ctx.load(table);
+var rows = ctx.workbook.tables.getItem("Table1").tableRows;
+ctx.load(rows);
 ctx.executeAsync().then(function () {
 	
-	var rows = table.tableRows;
-	ctx.load(rows);
-	ctx.executeAsync().then(function () {
-	  RichApiTest.log.comment(rows.items.length.toString());
-	  for (var i = 0; i < rows.items.length; i++){
+	RichApiTest.log.comment(rows.items.length.toString());
+	for (var i = 0; i < rows.items.length; i++){
 		
-		  RichApiTest.log.comment(rows.items[i].values[0][1]);
-		  var rng = rows.getItemAt(i).getRange();
+		RichApiTest.log.comment(rows.items[i].values[0][1]);
+		var rng = rows.getItemAt(i).getRange();
 		
-	  	if (rows.items[i].values[0][1] > 2){
-			  rng.format.background.color = "#ff0000";
-		  }
-		  else{
-			  rng.format.background.color = "#00ff00";
-  		}
-		  ctx.executeAsync().then();
-	  }
-	});	
-});
+		if (rows.items[i].values[0][1] > 2){
+			rng.format.background.color = "#ff0000";
+		}
+		else{
+			rng.format.background.color = "#00ff00";
+		}
+		ctx.executeAsync().then();
+	}
+});	
