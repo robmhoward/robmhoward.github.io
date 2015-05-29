26e4ab7a2b186ca07bbbf97f95e6217bdc05687f
Trang <trang@luunguyen.net>
2015-05-29 14:33:09 -0700

    Fix Typo

--- word/samples/insertOoxml.js
+++ word/samples/insertOoxml.js
@@ -4,7 +4,7 @@ var range = ctx.document.selection;
 var ooxmlText =
     "<w:p xmlns:w='http://schemas.microsoft.com/office/word/2003/wordml'><w:r><w:rPr><w:b/><w:b-cs/><w:color w:val='FF0000'/><w:sz w:val='28'/><w:sz-cs w:val='28'/></w:rPr><w:t>Hello world (this should be bold, red, size 14).</w:t></w:r></w:p>";
 
-range.insertOoxml(ooxmlText, .InsertLocation.end);
+range.insertOoxml(ooxmlText, Word.InsertLocation.end);
 
 ctx.executeAsync().then(
      function () {
