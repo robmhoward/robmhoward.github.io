239975bbba05506ae5edd96c0d12753ca77b92de
Trang <trang@luunguyen.net>
2015-06-05 16:38:35 -0700

    Add content control samples

--- word/samples/setContentControls.js
+++ word/samples/setContentControls.js
@@ -1,16 +1,22 @@
 var ctx = new Word.WordClientContext();
-var range = ctx.document.selection;
+ctx.customData = OfficeExtension.Constants.iterativeExecutor;
 
-var myContentControl = range.insertContentControl();
-
-ctx.load(myContentControl);
+var paras = ctx.document.body.paragraphs;
+ctx.load(paras);
 
 ctx.executeAsync().then(
-     function () {
-         console.log("Content control Id: " + myContentControl.id);
-     },
-     function (result) {
-         console.log("Failed: ErrorCode=" + result.errorCode + ", ErrorMessage=" + result.errorMessage);
-         console.log(result.traceMessages);
-     }
+    function () {
+        for (var i = 0; i < paras.count; i++) {
+            paras.items[i].insertContentControl();
+        }
+        ctx.executeAsync().then(
+            function () {
+                console.log("Success");
+            }
+        );
+    },
+    function (result) {
+        console.log("Failed: ErrorCode=" + result.errorCode + ", ErrorMessage=" + result.errorMessage);
+        console.log(result.traceMessages);
+    }
 );
