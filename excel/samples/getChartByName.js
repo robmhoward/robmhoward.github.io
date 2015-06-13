b4510250d3786bcbe4e1898b145b742e16fea722
Rob Howard <robmhoward@hotmail.com>
2015-06-12 17:24:24 -0700

    Sample updates and cleanup

--- excel/samples/getChartByName.js
+++ excel/samples/getChartByName.js
@@ -3,4 +3,5 @@ var chart = ctx.workbook.worksheets.getItem("Sheet1").charts.getItem("Chart1");
 ctx.load(chart);
 ctx.executeAsync().then(function () {
 		console.log(chart.name);
+		console.log("done");
 });
