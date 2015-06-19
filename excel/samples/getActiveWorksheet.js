7ea37b036bcae8d443552da27f7c26457e131b12
skyms <ylu@microsoft.com>
2015-06-18 19:18:05 -0700

    adding get/delete activeworksheet back

--- excel/samples/getActiveWorksheet.js
+++ excel/samples/getActiveWorksheet.js
@@ -1,5 +1,5 @@
 var ctx = new Excel.ExcelClientContext();
-var activeWorksheet = ctx.workbook.getActiveWorksheet();
+var activeWorksheet = ctx.workbook.worksheets.getActiveWorksheet();
 ctx.load(activeWorksheet);
 ctx.executeAsync().then(function () {
 	console.log(activeWorksheet.name);
