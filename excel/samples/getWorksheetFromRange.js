0fef633c121aeaef7e8c0ebdb75293affa25d33f
Jakob Nielsen <jakobpn@microsoft.com>
2015-06-30 11:57:24 -0700

    Added getWorksheetFromRange sample

---
+++ excel/samples/getWorksheetFromRange.js
@@ -0,0 +1,8 @@
+var ctx = new Excel.ExcelClientContext();
+var selectedRange = ctx.workbook.getSelectedRange();
+ctx.load(selectedRange.worksheet);
+ctx.executeAsync().then(function () {
+    console.log(selectedRange.worksheet.name);
+}, function (error) {
+    console.log("An error occurred: " + error.errorCode + ":" + error.errorMessage);
+});
